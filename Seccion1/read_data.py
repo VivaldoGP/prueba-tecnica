5f4713a206d6cedaa1c84ffc625021c0f4993691
Vivaldo <VivaldoGP27@hotmail.com>
2022-12-13 04:06:32 -0600

    Se añade la conexión a la base de datos

--- Seccion1/read_data.py
+++ Seccion1/read_data.py
@@ -1,20 +1,26 @@
 from csv import reader, writer
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from os import getenv
 
-def prepareData(inFilename: str, outFilename: str):
+load_dotenv()
+
+def prepareData(inFile: str, outFile: str):
     """
     Esta función toma como argumento un archivo de preferencia CSV
     y devuelve el mismo archivo sin renglones vacíos.
 
     Args:
-        filename (str): Ruta del archivo a procesar.
-        outFilename (str): Ruta del archivo procesado.
+        inFile (str): Ruta del archivo a procesar.
+        outFile (str): Ruta del archivo procesado.
     """    
 
     try:
-        with open(inFilename, mode="r") as rawFile:
+        with open(inFile, mode="r") as rawFile:
             csvReader = reader(rawFile, delimiter=',')
 
-            with open(outFilename, mode="w", newline='') as preparedFile:
+            with open(outFile, mode="w", newline='') as preparedFile:
                 csvWriter = writer(preparedFile, delimiter=',')
                 for row in csvReader:
                     if len(row) > 0:
@@ -26,4 +32,45 @@ def prepareData(inFilename: str, outFilename: str):
 
         return print("Se ha modificado tu archivo")
 
-prepareData("../Dataset/data_prueba_tecnica.csv", "../Seccion1/results/data_prueba_tecnica_cleaned.csv")
+#prepareData("../Dataset/data_prueba_tecnica.csv", "../Seccion1/results/data_prueba_tecnica_cleaned.csv")
+
+def nullData(inFile):
+    data = pd.read_csv(inFile)
+    columns = data.columns.values.tolist()
+
+    nullDict = {}
+    for col in columns:
+        nullDict[col] = data[col].isna().sum()
+        
+
+    return nullDict
+    
+
+def getConnection():
+    
+    dialect = getenv('DIALECT')
+    username = getenv('USERNAME')
+    password = getenv('PASSWORD')
+    host = getenv('HOST')
+    port = getenv('PORT')
+    database = getenv('DATABASE')
+
+    conn = create_engine(f"{dialect}://{username}:{password}@{host}:{port}/{database}")
+
+    return conn
+
+def exportDataToDB(inFile: str, naColumns: list, expColumns: list, con):
+
+    df = pd.read_csv(inFile)
+    df = df.dropna(subset=naColumns)
+
+    return print(df[expColumns].shape)
+    
+
+
+#nullData("../Seccion1/results/data_prueba_tecnica_cleaned.csv")
+
+
+#exportDataToDB(inFile="../Seccion1/results/data_prueba_tecnica_cleaned.csv", naColumns=["id"], expColumns=["id", "name", "company_id"])
+
+getConnection()
