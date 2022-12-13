62ef99cfd9a592d4bb1d7fa563f93f25bc7ce3db
Vivaldo <VivaldoGP27@hotmail.com>
2022-12-13 04:07:28 -0600

    Estructura básica de la base de datos

---
+++ Seccion1/DB/Schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS companies (
+    company_id VARCHAR(45) PRIMARY KEY NOT NULL,
+    company_name VARCHAR(15) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS charges (
+    id_charge VARCHAR(45) PRIMARY KEY NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    status VARCHAR(30) NOT NULL,
+    created_at DATE NOT NULL,
+    paid_at DATE
+);
+
+CREATE TABLE IF NOT EXISTS charges_company (
+    id_charge VARCHAR(45) PRIMARY KEY NOT NULL,
+    company_id VARCHAR(45),
+    company_name VARCHAR(15)
+);
+
+ALTER TABLE charges_company
+ADD CONSTRAINT fk_charges_company_id
+FOREIGN KEY (company_id)
+REFERENCES companies (company_id)
+ON DELETE CASCADE;
+
+ALTER TABLE charges_company
+ADD CONSTRAINT fk_charges_company_name
+FOREIGN KEY (company_name)
+REFERENCES companies (company_name)
+ON DELETE CASCADE;
+
+ALTER TABLE charges_company
+ADD CONSTRAINT fk_id_charges_company
+FOREIGN KEY (id_charge)
+REFERENCES charges (id_charge)
+ON DELETE CASCADE;
---
+++ Seccion1/DB/createDB.sql
@@ -0,0 +1,6 @@
+
+CREATE DATABASE prueba_tecnica_vivaldo
+WITH
+    ENCODING = 'UTF8'
+    OWNER = postgres
+    CONNECTION LIMIT = -1;
